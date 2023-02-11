5f036fac2456c4843649e6940dc25ed68a1fe987
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2023-02-11 10:07:28 +0700

    Create Introduction course

---
+++ app/src/main/java/com/example/learncodeapp/IntroductionCourse.java
@@ -0,0 +1,14 @@
+package com.example.learncodeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroductionCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction_course);
+    }
+}
