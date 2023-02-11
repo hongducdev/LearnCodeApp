62ca1843c15fb60a50ecc65212197e878b2ab11b
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2023-02-11 19:59:38 +0700

    Update Interface QuestionPage

--- app/src/main/java/com/example/learncodeapp/Question.java
+++ app/src/main/java/com/example/learncodeapp/Question.java
@@ -6,18 +6,38 @@ import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.TextView;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Question extends AppCompatActivity {
     TextView nameBar;
+    LinearLayout questionLayout;
+    TextView questionName;
+    RadioButton answer1, answer2, answer3, answer4;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         // Change header
 //        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
@@ -38,7 +58,33 @@ public class Question extends AppCompatActivity {
                 finish();
             }
         });
+
+        questionLayout = findViewById(R.id.questionLayout);
+        questionName = findViewById(R.id.questionName);
+        answer1 = findViewById(R.id.answer1);
+        answer2 = findViewById(R.id.answer2);
+        answer3 = findViewById(R.id.answer3);
+        answer4 = findViewById(R.id.answer4);
+
+        db.collection("questions").document("html-basic")
+                .collection("questions")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                                questionName.setText(document.getString("question"));
+                                answer1.setText(document.getString("answer1"));
+                                answer2.setText(document.getString("answer2"));
+                                answer3.setText(document.getString("answer3"));
+                                answer4.setText(document.getString("answer4"));
+                            }
+                        } else {
+                            Log.d("TAG", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
-
-
 }
