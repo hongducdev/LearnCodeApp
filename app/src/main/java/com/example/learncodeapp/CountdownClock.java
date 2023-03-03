90dae2ca27aa533b58881c7b6b041d486a7da9ad
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2023-03-03 17:24:07 +0700

    Done countdown timer

--- app/src/main/java/com/example/learncodeapp/CountdownClock.java
+++ app/src/main/java/com/example/learncodeapp/CountdownClock.java
@@ -3,12 +3,67 @@ package com.example.learncodeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CountdownClock extends AppCompatActivity {
+
+    TextView tvCountdownMinute, tvCountdownSecond;
+    Button btnStart, btnReset, btn25, btn45, btn60;
+    CountDownTimer Timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_countdown_clock);
+
+        tvCountdownMinute = findViewById(R.id.tvCountdownMinute);
+        tvCountdownSecond = findViewById(R.id.tvCountdownSecond);
+        btnStart = findViewById(R.id.btnStart);
+        btnReset = findViewById(R.id.btnReset);
+        btn25 = findViewById(R.id.btn25);
+        btn45 = findViewById(R.id.btn45);
+        btn60 = findViewById(R.id.btn60);
+
+        btn25.setOnClickListener(v -> {
+            tvCountdownMinute.setText("25");
+            tvCountdownSecond.setText("00");
+        });
+
+        btn45.setOnClickListener(v -> {
+            tvCountdownMinute.setText("45");
+            tvCountdownSecond.setText("00");
+        });
+
+        btn60.setOnClickListener(v -> {
+            tvCountdownMinute.setText("60");
+            tvCountdownSecond.setText("00");
+        });
+
+        btnStart.setOnClickListener(v -> {
+            Timer = new CountDownTimer(Integer.parseInt(tvCountdownMinute.getText().toString()) * 60 * 1000, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    int minutes = (int) (millisUntilFinished / 1000) / 60;
+                    int seconds = (int) (millisUntilFinished / 1000) % 60;
+                    tvCountdownMinute.setText(String.format("%02d", minutes));
+                    tvCountdownSecond.setText(String.format("%02d", seconds));
+                }
+
+                @Override
+                public void onFinish() {
+                    tvCountdownMinute.setText("00");
+                    tvCountdownSecond.setText("00");
+                }
+            }.start();
+        });
+
+        btnReset.setOnClickListener(v -> {
+            Timer.cancel();
+            tvCountdownMinute.setText("25");
+            tvCountdownSecond.setText("00");
+        });
+
     }
 }
